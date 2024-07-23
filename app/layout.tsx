a6243afbd1c80c56d6fd41f4b2dccbc9a7d9f868
Shoaib Jalal <syedshoaibjalal@outlook.com>
2024-07-23 21:50:48 +0300

    passcode funtionality done on frontend

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Asap } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
-import { ThemeProvider } from "../components/theme-provider";
+import { ThemeProvider } from "@/components/theme-provider"
 
 const fontSans = Asap({
   subsets: ["latin"],
@@ -24,14 +24,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={cn(
           "min-h-screen bg-dark-300 font-sans antialiased",
           fontSans.variable
         )}
       >
-        <ThemeProvider attribute="class" defaultTheme="dark">
+        <ThemeProvider attribute="class" defaultTheme="dark" >
           {children}
         </ThemeProvider>
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import Link from "next/link";
 import Image from "next/image";
 import PatientForm from "@/components/forms/PatientForm";
+import { PasskeyModal } from "@/components/PasskeyModal";
 
-export default function Home() {
+const Home = ({ searchParams }: SearchParamProps) => {
+  const isAdmin = searchParams?.admin === "true";
   return (
     <div className=" flex h-screen max-h-screen">
+          {isAdmin && <PasskeyModal />}
       <section className="remove-scrollbar container my-auto">
+    
         <div className="sub-container max-w-[496px]">
           <Image
             src="/assets/icons/logo-full.svg"
@@ -14,6 +18,7 @@ export default function Home() {
             alt="patient"
             className="mb-12 h-10 w-fit"
           />
+        
           <PatientForm />
           <div className="text-14-regular mt-20 flex justify-between">
             <p className="justify-items-end text-dark-600 xl:text-left">
@@ -35,3 +40,4 @@ export default function Home() {
     </div>
   );
 }
+export default Home;
